e6812c774da6c18ae60056085c5bc00b85bf689d
ozonations <104888650+ozonations@users.noreply.github.com>
2022-08-10 09:44:28 +0700

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ozonations/nutriproject
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	ns := GetNutritionalScore(NutritionalData{
+		Energy:              EnergyFromKcal(100),
+		Sugars:              SugarGram(10),
+		SaturatedFattyAcids: SaturatedFattyAcids(2),
+		Sodium:              SodiumMiligram(500),
+		Fruits:              FruitsPercent(60),
+		Fibre:               FibreGram(4),
+		Protein:             ProteinGram(2),
+	}, Food)
+
+	fmt.Printf("Nutritional Score:%d\n", ns.Value)
+	fmt.Printf("NutriScore: %s\n", ns.GetNutritionalScore())
+}
